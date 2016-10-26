c77b762bf34c7398fba3a337a63a442b18073d4c
jane2017 <janec2100@gmail.com>
2016-10-26 18:30:43 -0400

    updated app.js

--- assignment1-starter-code/app/app.js
+++ assignment1-starter-code/app/app.js
@@ -1,10 +1,27 @@
-angular.module('LunchCheck',[])
-.controller('LunchCheckController', LunchCheckController);
-
-function LunchCheckController ($scope) {
-    $scope.menu='';
-    $scope.getDefaultVal=function($scope){
-        return 'list comma separated dishes you usually have for lunch';
-    };
+(function () {
+    'use strickt';
     
-}
+    angular.module('LunchCheck',[])
+    .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope', '$filter'];
+    function LunchCheckController ($scope, $filter) {
+        $scope.menu='';
+        $scope.checkResult='';
+        $scope.getDefaultVal=function($scope){
+            return 'list comma separated dishes you usually have for lunch';
+        };
+        console.log($scope.menu);
+        
+        $scope.checkIfTooMuch = function () {
+            var menuItems = $scope.menu.split(',');
+            if (menuItems.length <= 3) {
+                $scope.checkResult = 'enjoy!';
+            }
+            else {
+                $scope.checkResult = 'too much!';
+            }
+        };
+            
+    };
+})();
