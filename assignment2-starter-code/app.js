582359b485cbadf0771a6e8b4db91c0d40582600
jane2017 <janec2100@gmail.com>
2016-10-31 18:19:17 -0400

    test code.

--- assignment2-starter-code/app.js
+++ assignment2-starter-code/app.js
@@ -2,17 +2,46 @@
   'use strict';
   angular.module('ShoppingListCheckOff',[])
   .controller('ToBuyController', ToBuyController)
-  .controller('AlreadyBNoughtController', AlreadyBNoughtController);
+  .controller('AlreadyBNoughtController', AlreadyBNoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
   
-  AlreadyBNoughtController.$interpolate=['$scope'];
-  ToBuyController.$interpolate=['$scope'];
   
-  function AlreadyBNoughtController ($scope) {
-    
+  AlreadyBNoughtController.$inject=['ShoppingListCheckOffService'];
+  function AlreadyBNoughtController (ShoppingListCheckOffService) {
+    var service = ShoppingListCheckOffService;
+    var boughtItem = function(index) {
+      return service.boughtIrem(index);
+    };
   }
   
-  function ToBuyController($scope) {
+  ToBuyController.$inject=['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var service = ShoppingListCheckOffService;
+
+    var getToBuyList = function() {
+      return service.getToBuyList();
+    }
+  }
     
+
+  
+  ShoppingListCheckOffService.$inject=[];
+  function ShoppingListCheckOffService() {
+    var toBuy=['Buy 10 cookies','Buy 20 cookies'];
+    var bought=[];
+    
+    var boughtItem = function (index) {
+      bought.push(toBuy[index]);
+      toBuy.remove(index);
+    };
+    
+    var getToBuyList = function() {
+      return toBuy;
+    };
+    
+    var getBoughtList = function() {
+      return bought;
+    };
   }
     
 })();
